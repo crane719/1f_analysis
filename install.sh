bdd6978a276f9210bb061a314682857edc61766d
crane719 <tmp.utils@gmail.com>
2020-12-16 03:01:15 +0900

    1/fの閾値を決定する画面追加

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ source venv/bin/activate
 pip install -r requirements.txt
 #brew install ffmpeg --with-libvorbis --with-sdl2 --with-theora
 brew install ffmpeg
+mkdir dataset
---
+++ run.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, jsonify
+import json
+from collections import OrderedDict
+
+# app, jinjaの設定変更
+app=Flask(__name__)
+jinja_options=app.jinja_options.copy()
+jinja_options.update({
+    "variable_start_string": "<<",
+    "variable_end_string": ">>",
+    })
+app.jinja_options=jinja_options
+
+f=open("result.json", "r")
+result_dict=json.load(f)
+
+# directoryをkey, 標準偏差をvalueとしたorderddict(sorted)を作成
+tmps={}
+for _, v in result_dict.items():
+    for music_dir, v1 in v.items():
+        music_dir=music_dir.split("/")
+        if len(music_dir)==3:
+            music_dir[2]="log_"+music_dir[2].split(".")[0]+".png"
+        tmps["static/analysis_result/fitting/"+music_dir[1]+"/"+music_dir[2]]=v1["std"]
+"""
+pic_dict=OrderedDict((k, v) for k, v in sorted(tmps.items(), key=lambda item: item[1]))
+"""
+pic_dict={"dirs":[], "std":[]}
+for k, v in sorted(tmps.items(), key=lambda item: item[1]):
+    pic_dict["dirs"].append(k)
+    pic_dict["std"].append(v)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/get_pic")
+def get_pic_dir():
+    return jsonify(pic_dict)
+
+if __name__=="__main__":
+    app.run(debug=True)
+
